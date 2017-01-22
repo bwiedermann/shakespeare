060c7cc0df55911d5d67b92f546c064b64b189b7
Ben Wiedermann <benw@cs.hmc.edu>
2017-01-21 23:43:33 -0800

    Replacing “Reader” with “About” and giving credit to the implementers.

--- static/plays/navBarService.js
+++ static/plays/navBarService.js
@@ -1,38 +1,37 @@
-
 /**
-   * Plays DataService
-   * Uses embedded, hard-coded data model; acts asynchronously to simulate
-   * remote data service call(s).
-   *
-   * @returns {{loadAll: Function}}
-   * @constructor
-   */
-function NavBarService($q){
+ * Plays DataService
+ * Uses embedded, hard-coded data model; acts asynchronously to simulate
+ * remote data service call(s).
+ *
+ * @returns {{loadAll: Function}}
+ * @constructor
+ */
+function NavBarService($q) {
     var features = [
-      {
-        name: 'Summary',
-        avatar: 'svg-1'
+        {
+            name: 'Summary',
+            avatar: 'svg-1'
       },
-      {
-        name: 'Visualizations',
-        avatar: 'svg-2'
+        {
+            name: 'Visualizations',
+            avatar: 'svg-2'
       },
-      {
-        name: 'Character List',
-        avatar: 'svg-3'
+        {
+            name: 'Character List',
+            avatar: 'svg-3'
       },
-      {
-        name: 'Reader',
-        avatar: 'svg-4',
+        {
+            name: 'About',
+            avatar: 'svg-4',
       }
     ];
 
     // Promise-based API
     return {
-      loadAllFeatures : function() {
-        // Simulate async nature of real remote calls
-        console.log(features);
-        return $q.when(features);
-      }
+        loadAllFeatures: function () {
+            // Simulate async nature of real remote calls
+            console.log(features);
+            return $q.when(features);
+        }
     };
-  }
+}
